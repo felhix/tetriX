706e353d2e8e86b037444140b64f13b77a79e94a
Félix Gaudé <felix.gaude@gmail.com>
2025-03-28 19:16:51 +0100

    Initial commit

---
+++ godot/autoload/reference/asset_reference/asset_reference.gd
@@ -0,0 +1,15 @@
+extends Node
+## Preload explicitly assets here for quick access.
+## [br][br]
+## Original File MIT License Copyright (c) 2024 TinyTakinTeller
+
+# Music
+const MENU_DOODLE_2_LOOP: AudioStream = preload(
+	"res://assets/audio/music/menu_doodle_2_loop/ogg/menu_doodle_2_loop.ogg"
+)
+
+# SFX
+const CLICK_4: AudioStream = preload("res://assets/audio/sfx/kenny_ui/ogg/click4.ogg")
+const CLICK_5: AudioStream = preload("res://assets/audio/sfx/kenny_ui/ogg/click5.ogg")
+const MOUSECLICK_1: AudioStream = preload("res://assets/audio/sfx/kenny_ui/ogg/mouseclick1.ogg")
+const MOUSERELEASE_1: AudioStream = preload("res://assets/audio/sfx/kenny_ui/ogg/mouserelease1.ogg")
