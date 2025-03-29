eb3d63b5f5908337b976793d49f87b275c181f66
Douglas DUTEIL <douglasduteil@gmail.com>
2025-03-29 15:57:31 +0100

    shiny scene

--- godot/scenes/main-scene/core.gd
+++ godot/scenes/main-scene/core.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 @export var rotation_speed = PI / 2;
 
+@onready var layer: TileMapLayer = $TileMapLayer
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -14,8 +16,8 @@ func _process(delta: float) -> void:
 	elif Input.is_action_pressed("ui_right"):
 		rotate(delta * -rotation_speed);
 		
-#func merge_with_core(position: Vector2i):
-
+	merge_with_core(Vector2i(randi_range(-10, 10), randi_range(-10, 10)))
 	
-
-	
+func merge_with_core(position: Vector2i) -> void:
+	layer.set_cell(position, 0, Vector2i(randi_range(0, 7), 0))
+	# layer.set_cell(position, 0, Vector2i.ZERO)
