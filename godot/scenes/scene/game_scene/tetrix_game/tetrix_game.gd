c69f64adc8deccff719ebbb0b9135c0cdbf095c3
Douglas DUTEIL <douglasduteil@gmail.com>
2025-03-29 11:54:58 +0100

    add block spawner

--- godot/scenes/scene/game_scene/tetrix_game/tetrix_game.gd
+++ godot/scenes/scene/game_scene/tetrix_game/tetrix_game.gd
@@ -1,5 +1,23 @@
 class_name TetrixGame
 extends Node2D
 
+@onready var spawner: BlockSpawner  = $Spawner
+
+var falling_block: Block = null;
+
 func _ready() -> void:
 	LogWrapper.debug(self, "Tetrixxxxxxxx ready.")
+
+func _process(delta: float) -> void:
+	#if Input.is_action_pressed("ui_left"):
+		#steps[0] += 10
+	#elif Input.is_action_pressed("ui_right"):
+		#steps[1] += 10
+	#elif Input.is_action_pressed("ui_down"):
+		#steps[2] += 10
+	#elif Input.is_action_just_pressed("ui_up"):
+		#spawner.rotate_piece()
+
+
+	if not falling_block:
+		falling_block = spawner.instanciate_new_block()
